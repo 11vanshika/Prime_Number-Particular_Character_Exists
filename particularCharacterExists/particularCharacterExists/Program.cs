c3eab00da22f59f203b1ff527b788288d243a9df
11vanshika <vanshikaa@moback.com>
2022-09-28 10:10:17 +0530

    first commit

---
+++ particularCharacterExists/particularCharacterExists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace particularCharacterExists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter string");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter Character which you find");
+             char checkValue = Convert.ToChar(Console.ReadLine());
+
+            char[] chars = str1.ToCharArray();
+            for(int i = 0; i < chars.Length; i++)
+            {
+                if(chars[i] == checkValue)
+                {
+                    Console.WriteLine("Character is present :" + i);
+                }
+            }
+            Console.WriteLine("Character is not present");
+        }
+    }
+}
---
+++ primeNumber/primeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            Console.Write("1-100 Prime number:");
+            for(int number = 1; number <= 100; number++)
+            {
+                count =0;
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                    if (count == 2)
+                    {
+                        Console.Write(number + ",");
+                    }
+            }
+        }
+    }
+}
